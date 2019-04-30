b4bfee2a43960825ee4c4add5aafcf0ff6d53871
alt3ralus <47127251+alt3ralus@users.noreply.github.com>
2019-04-30 14:15:29 -0500

    cambio para poder subir y bajar lineas con ALT

--- core/core-keys.el
+++ core/core-keys.el
@@ -28,8 +28,8 @@
  )
 )
 
-(global-set-key (kbd "M-<up>") 'move-text-up)
-(global-set-key (kbd "M-<down>") 'move-text-down)
+(global-set-key (kbd "<ESC><up>") 'move-text-up)
+(global-set-key (kbd "<ESC><down>") 'move-text-down)
 
 (global-set-key (kbd "M-S-<up>") 'mc/mark-previous-like-this)
 (global-set-key (kbd "M-S-<down>") 'mc/mark-next-like-this)
